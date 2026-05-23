1986eefca3f04f38618f1aa061c120f2d09fdaf7
Samuel Zambrano <szambranogarza@gmail.com>
2026-05-23 15:49:46 -0600

    testing data, will explore json files

---
+++ tests/transform_pokemon_wip.py
@@ -0,0 +1,20 @@
+import json
+
+file_path = "data/raw/pokemon_1.json"
+
+with open(file_path, "r") as file:
+    data = json.load(file)
+
+pokemon_id = data['id']
+name = data['name']
+height = data['height']
+weight = data['weight']
+base_experience = data['base_experience']
+is_default = data['is_default']
+
+print(pokemon_id)
+print(name)
+print(height)
+print(weight)
+print(base_experience)
+print(is_default)
