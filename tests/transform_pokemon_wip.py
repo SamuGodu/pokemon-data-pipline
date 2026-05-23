de67b72058595d42d172b7db647b1a89fef1d9fc
Samuel Zambrano <szambranogarza@gmail.com>
2026-05-23 16:50:55 -0600

    Capturing dim_abilities

--- tests/transform_pokemon_wip.py
+++ tests/transform_pokemon_wip.py
@@ -5,6 +5,8 @@ file_path = "data/raw/pokemon_1.json"
 with open(file_path, "r") as file:
     data = json.load(file)
 
+
+# dim_pokemon
 pokemon_id = data['id']
 name = data['name']
 height = data['height']
