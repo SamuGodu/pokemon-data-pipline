a90dbd7d8d7f9a1635dd3dc037549768f49bf4f4
Samuel Zambrano <szambranogarza@gmail.com>
2026-05-23 15:26:22 -0600

    Added loading script and inserted data into Postgres

--- src/load/load_raw_response.py
+++ src/load/load_raw_response.py
@@ -1,14 +1,57 @@
-import json 
-import os 
-import psycopg2 
+import json
+import os
+import psycopg2
 from dotenv import load_dotenv
-
-
-
-
 
 load_dotenv()
 
-print("DB:", os.getenv("POSTGRES_DB"))
-print("HOST:", os.getenv("POSTGRES_HOST"))
-print("PORT:", os.getenv("POSTGRES_PORT"))
+conn = psycopg2.connect(
+    host=os.getenv("POSTGRES_HOST"),
+    port=os.getenv("POSTGRES_PORT"),
+    database=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD")
+)
+
+cursor = conn.cursor()
+
+raw_folder = "data/raw"
+
+for filename in os.listdir(raw_folder):
+    if filename.endswith(".json"):
+        file_path = os.path.join(raw_folder, filename)
+
+        with open(file_path, "r") as file:
+            data = json.load(file)
+
+        pokemon_id = data["id"]
+        url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
+
+        cursor.execute(
+            """
+            INSERT INTO raw_api_responses (
+                endpoint,
+                resource_id,
+                url,
+                response_json,
+                status_code
+            )
+            VALUES (%s, %s, %s, %s, %s)
+            ON CONFLICT (endpoint, resource_id) DO NOTHING;
+            """,
+            (
+                "pokemon",
+                pokemon_id,
+                url,
+                json.dumps(data),
+                200
+            )
+        )
+
+        print(f"Inserted raw response for Pokemon {pokemon_id}")
+
+conn.commit()
+cursor.close()
+conn.close()
+
+print("Raw JSON load complete.")
