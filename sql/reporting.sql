4367c6b5afd284e14710fb5447bc16fca981ef90
Samuel Zambrano <szambranogarza@gmail.com>
2026-05-25 15:48:27 -0600

    Answered question 2 of reporting

--- sql/reporting.sql
+++ sql/reporting.sql
@@ -6,3 +6,12 @@ ON ft.pokemon_id = p.pokemon_id
     JOIN dim_type t
     ON ft.type_id = t.type_id
 GROUP BY t.name
+
+-- Average hieght and weight by type
+SELECT t.name, ROUND(AVG(p.weight),2) as Average_weight , ROUND(AVG(p.height),2) as Average_height
+FROM dim_pokemon p
+JOIN fact_pokemon_type ft
+ON ft.pokemon_id = p.pokemon_id
+    JOIN dim_type t
+    ON ft.type_id = t.type_id
+GROUP BY t.name
